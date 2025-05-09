df30bb5d41d7f8da4ca51f0d18426c989d3370ee
Mathias Tenemas <1542422@senati.pe>
2025-05-09 00:25:16 -0500

    db connect 3

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,7 +1,8 @@
 // src/config/apiConfig.js
 
 // Define la URL base de tu API aquí
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 // Puedes exportar también otras constantes relacionadas con la API si es necesario
 // const API_TIMEOUT = 10000; // Ejemplo de timeout
