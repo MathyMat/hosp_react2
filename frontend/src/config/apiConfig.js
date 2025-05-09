3f2ef9159f468fc7b165087bd1c0dd20728f859f
Mathias Tenemas <1542422@senati.pe>
2025-05-09 00:18:22 -0500

    error db connect

--- frontend/src/config/apiConfig.js
+++ frontend/src/config/apiConfig.js
@@ -1,7 +1,7 @@
 // src/config/apiConfig.js
 
 // Define la URL base de tu API aquí
-const API_BASE_URL = process.env.REACT_APP_API_URL;
+const API_BASE_URL = process.env.API_BASE_URL;
 
 // Puedes exportar también otras constantes relacionadas con la API si es necesario
 // const API_TIMEOUT = 10000; // Ejemplo de timeout
